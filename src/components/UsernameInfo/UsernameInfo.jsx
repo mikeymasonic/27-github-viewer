318f3a2d79056cdffd6e9a5d0f4c9aec1dc68d36
Michael Romay <amerror@gmail.com>
2020-04-28 15:24:15 -0700

    usernameinfo

---
+++ src/components/UsernameInfo/UsernameInfo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UsernameInfo = ({ avatar_url, login, followers, following, html_url }) => (
+  <figure>
+    <img src={avatar_url} alt={login} />
+    <figcaption>
+      <p>{login}</p>
+      <p>{followers}</p>
+      <p>{following}</p>
+      <p><a href={html_url}></a></p>
+    </figcaption>
+  </figure>
+);
+
+UsernameInfo.propTypes = {
+  avatar_url: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  following: PropTypes.number.isRequired,
+  html_url: PropTypes.string.isRequired,
+};
+
+export default UsernameInfo;
